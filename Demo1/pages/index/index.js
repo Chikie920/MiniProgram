3d89eee654fc14ef1daf76c544e26af23ed31857
Chikie920 <livewild0920@gmail.com>
2022-06-21 22:34:40 +0800

    update

--- Demo1/pages/index/index.js
+++ Demo1/pages/index/index.js
@@ -4,6 +4,7 @@ const app = getApp();
 Page({
   data:{
     naviHeight: app.globalData.naviHeight,
-    screenHeight: app.globalData.screenHeight-app.globalData.naviHeight
+    screenHeight: app.globalData.screenHeight-app.globalData.naviHeight,
+    screenWidth: app.globalData.screenWidth
   }
 })
