6a7f9697bd009e6d3af51224f1221a65ba1ec183
Chikie920 <livewild0920@gmail.com>
2022-06-26 20:38:03 +0800

    update

--- Demo2/pages/index/index.js
+++ Demo2/pages/index/index.js
@@ -5,6 +5,14 @@ Page({
   data:{
     naviHeight: app.globalData.naviHeight,
     screenHeight: app.globalData.screenHeight-app.globalData.naviHeight,
-    screenWidth: app.globalData.screenWidth
+    screenWidth: app.globalData.screenWidth,
+    lang: ["Lang, ZH", "Lang, EN"],
+    lang_change: 0
+  },
+  change_lang: function(event) {
+    var change = (this.data.lang_change+1)%2;
+    this.setData({
+      lang_change: change
+    })
   }
 })
