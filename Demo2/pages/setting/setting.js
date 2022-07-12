9c55fd46a4db930fbe97bd91546e12d23514262b
Chikie920 <livewild0920@gmail.com>
2022-07-12 22:25:13 +0800

    update

--- Demo2/pages/setting/setting.js
+++ Demo2/pages/setting/setting.js
@@ -11,7 +11,7 @@ Page({
     naviHeight: app.globalData.naviHeight,
     screenHeight: app.globalData.screenHeight-app.globalData.naviHeight,
     screenWidth: app.globalData.screenWidth,
-    openid: app.globalData.openid,
+    user_openid: '',
     avatar_url: ''
   },
 
@@ -19,18 +19,25 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    let that = this;
-    console.error(this.data.openid);
-    db.collection('user_data').where({
-      _openid: this.data.openid
-    }).get({
+    var that = this;
+    wx.cloud.callFunction({
+      name: 'get_openid',
       success(res) {
-        that.setData({
-          avatar_url: res.data[0].avatar_url
+        // 获取openid,感觉有些冗余，考虑使用全局变量替换
+        db.collection('user_data').where({
+          _openid: res.result.openid
+        }).get({
+          success(res) {
+            that.setData({
+              user_openid: res.result.openid,
+              avatar_url: res.data[0].avatar_url
+            });
+            console.error(that.data);
+          }
         });
+        // 获取用户头像
       }
     });
-    // 获取用户头像
   },
 
   /**
